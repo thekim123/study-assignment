5330814b7d8854b9d18b5055ead1cc85e4bbd2fd
thekim123 <tacdo12@naver.com>
2023-02-13 21:07:26 +0900

    02/13 과제 재

--- src/No120903.java
+++ src/No120903.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class No120903 {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class No120903 {
+
+    public static void main(String[] args) throws IOException {
+        Solution1 s = new Solution1();
+        String[] s1 = {"a", "b", "c"};
+        String[] s2 = {"com", "b", "d", "p", "c"};
+        System.out.println(s.solution(s1, s2));
+
+        String[] s3 = {"n", "omg"};
+        String[] s4 = {"m", "dot"};
+        System.out.println(s.solution(s3, s4));
+
+    }
+
 }
+
+class Solution1 {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        List<Set> arr = new ArrayList<>();
+        Set<String> set = new HashSet<>();
+        for (String s : s1) {
+            set.add(s);
+        }
+        for (String s : s2) {
+            if (set.contains(s)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
+
+
--- src/No120907.java
+++ src/No120907.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class No120907 {
+public class No120907 {
+    public static void main(String[] args) {
+
+        String[] quiz1 = {"3 - 4 = -3", "5 + 6 = 11"};
+        String[] quiz2 = {"19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"};
+
+        Solution2 s = new Solution2();
+        for (String ans : s.solution(quiz1)) {
+            System.out.print(ans);
+        }
+        System.out.println();
+
+        for (String ans : s.solution(quiz2)) {
+            System.out.print(ans);
+        }
+    }
+
+}
+
+class Solution2 {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+        for (int i = 0; i < quiz.length; i++) {
+            String[] arr = quiz[i].split(" ");
+            if (arr[1].equals("-"))
+                answer[i] = Integer.parseInt(arr[0]) - Integer.parseInt(arr[2]) == Integer.parseInt(arr[4]) ? "O" : "X";
+            else
+                answer[i] = (Integer.parseInt(arr[0]) + Integer.parseInt(arr[2]) == Integer.parseInt(arr[4])) ? "O" : "X";
+        }
+        return answer;
+    }
 }
